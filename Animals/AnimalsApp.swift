ba085474605e7ffbf208f407a0ec15240d13767b
Aayam Adhikari <thisisleo7@gmail.com>
2023-08-16 17:24:29 +0545

    Initial Commit

---
+++ Animals/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Aayam Adhikari on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
